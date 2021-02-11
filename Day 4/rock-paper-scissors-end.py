a99d446cc563e7b09968faee30c8e2e103dd9e4a
iaminapickle <44413973+iaminapickle@users.noreply.github.com>
2021-02-11 18:48:47 +1100

    Create rock-paper-scissors-end.py

---
+++ Day 4/rock-paper-scissors-end.py
@@ -0,0 +1,31 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+choiceList = [rock, paper, scissors]
+
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(choiceList[choice])

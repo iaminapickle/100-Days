9d374372e88379641ea0dacb8bb0b11f1871f045
iaminapickle <44413973+iaminapickle@users.noreply.github.com>
2021-02-11 19:14:18 +1100

    Update rock-paper-scissors-end.py

--- Day 4/rock-paper-scissors-end.py
+++ Day 4/rock-paper-scissors-end.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -29,3 +31,15 @@ choiceList = [rock, paper, scissors]
 
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 print(choiceList[choice])
+
+print("Computer chose:\n")
+compChoice = random.randint(0,2)
+print(choiceList[compChoice])
+
+compBeats = (compChoice + 1) % 3
+if (compBeats == choice):
+    print("Win!")
+elif (compChoice == choice):
+    print("Tie!")
+else:
+    print("Loss!")
